23cbb5ab46128f4a452dcfbe18d53e1430b28af6
Rosanna Lam <gob@Rosannas-MacBook-Air.local>
2021-09-29 10:31:39 -0700

    testing

--- views.py
+++ views.py
@@ -1,27 +1,58 @@
+from math import radians
 from django.shortcuts import render
 import requests
 bars=[]
 # Create your views here.
-def home(request): 
+
+def home(request):
+        return render(request, 'apitest1/index.html',{
+    })
+def bar(request): 
+    # get location 
     response = requests.get('https://freegeoip.app/json/').json()
     lat = response['latitude']
     lon = response['longitude']
+    #get breweries attributed to location
     response2 = requests.get('https://api.openbrewerydb.org/breweries?by_dist='+str(lat)+','+str(lon)+"'").json()
     data={}
     k=0
+    #store response
     for item in response2:
+        dist = get_dist(lat, lon, float(item['latitude']), float(item['longitude']))
+        item['distance']= dist
+        pnum = item['phone']
+        pformatted = formatPhone(pnum)
+        item['phone']= pformatted
         data[k] = item
+
         k+=1
-        print(item['name']+ ', '+item['brewery_type'] )
-    return render(request, 'apitest1/index.html',{
+        #check responses
+        print(item['website_url'] )
+    #print to page
+    return render(request, 'apitest1/bar.html',{
         'barlist': data,
     })
 
+def get_dist(ulat, ulong, blat, blong):
+    import math
+    coordlat = ulat
+    coordlon = ulong
+    ulat, ulong, blat, blong = map(radians, [ulat, ulong, blat, blong])
+    coord2 = blat, blong
+    R = 6371000 #radius of Eath in meters
+    deltaphi = blat - ulat
+    deltalam = blong - ulong
+    a = math.sin(deltaphi/2.0)**2+ math.cos(ulat) * math.cos(blat)*math.sin(deltalam/2.0)**2
+    c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
+    miles = (R * c)* 0.000621371
+    return miles
 
-    def home2(request):
-        # someone makes an api request to YOUR api
-        # When that request comes in, you take THEIR IP Address
-        # Plug that IP Address into the Gecode API
-        # Serialize the result, and take the city portion and store it into a variable
-        # Plug the city portion into the brewery API
-        breweryResponse = requests.get('https://api.openbrewerydb.org/breweries?by_city=anaheim').json()
+def formatPhone(x):
+    if x is None:
+        return "No number listed"
+    elif len(x) != 10:
+        return "No number listed"
+    else:
+        phone = x[:3]+'-'+x[3:6]+'-'+x[6:]
+        return phone
+    #throw error
